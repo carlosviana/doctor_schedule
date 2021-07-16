610a94208275f5345e689c0e2f4002c638005d16
carlosviana <viana.mail@gmail.com>
2021-07-16 07:38:25 -0300

    ajustes credo

--- .credo.exs
+++ .credo.exs
@@ -3,14 +3,14 @@
     %{
       name: "default",
       files: %{
-        included: ~w{config lib test},
+        included: ~w{config lib test}
       },
       strict: true,
       color: true,
       checks: [
         {Credo.Check.Readability.ModuleDoc, false},
         {Credo.Check.Readability.MaxLineLength, max_length: 100},
-        {Credo.Check.Consistency.TabOrSpaces},
+        {Credo.Check.Consistency.TabsOrSpaces},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 100},
         {Credo.Check.Design.TagTODO, exit_status: 2},
         {Credo.Check.Refactor.MapInto, false},
